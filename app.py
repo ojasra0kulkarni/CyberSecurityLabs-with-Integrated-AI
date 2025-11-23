9d0be43852ed60bba1d8500fca766a93c16b1df4
Nandaprasad <71755831+nanduchowdary@users.noreply.github.com>
2025-11-23 23:17:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import datetime
-from tools import redblue  # Removed encryption import
+from tools import redblue  # Removed encryption 
 
 app = Flask(__name__)
 
